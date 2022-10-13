39c39618c44cbff0e414d2882c82c5db22d65acb
am_6733 <anton6733@gmail.com>
2022-10-13 07:36:02 +0300

    negative check

--- task/Program.cs
+++ task/Program.cs
@@ -1,6 +1,6 @@
 ﻿using static System.Console;
 WriteLine("первое задание:\n");
-string[] fast_test= {"aspera","Сорос","12321","456654","0123","0222","2220"};
+string[] fast_test= {"aspera","Сорос","12321","456654","0123","0222","2220","-00818"};
 Mirrored_string.todo();
 foreach(string var in fast_test){
     Mirrored_string run= new Mirrored_string(self:var);
